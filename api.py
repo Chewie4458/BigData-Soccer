0bdc4db0dba838078857f8d56520d7e6654f1357
Marcela <marcelavi.sul@gmail.com>
2025-10-30 21:27:58 -0300

    dados iniciais

--- api.py
+++ api.py
@@ -3,41 +3,63 @@ import requests
 import json
 from pandas import DataFrame
 
-conn = http.client.HTTPSConnection("v3.football.api-sports.io")
-
 headers = {
     'x-apisports-key': "2ca4f33e422f272a2021955472321911"
     }
 
-conn.request("GET", "/leagues", headers=headers)
-
-res = conn.getresponse()
-data = res.read()
-
-#print(data.decode("utf-8"))
-
-# Define o URL do endpoint
+''' INICIO PREDICTIONS '''
+# Define o URL do endpoint (predictions)
 url = "https://v3.football.api-sports.io/predictions?fixture=1477941"
 
-# Inicia o array de retorno
-payload={}
+# Realiza o request
+responsePredictions = requests.request("GET", url, headers=headers)
+
+# Converte a resposta para JSON
+dadosPredictions = json.loads(responsePredictions.text)
+''' FIM PREDICTIONS '''
+
+''' INICIO FIXTURES '''
+# Define o URL do endpoint (fixtures)
+url = "https://v3.football.api-sports.io/fixtures?id=1477941"
 
 # Realiza o request
-response = requests.request("GET", url, headers=headers, data=payload)
+responseFixtures = requests.request("GET", url, headers=headers)
 
-dados = json.loads(response.text)
+# Converte a resposta para JSON
+dadosFixtures = json.loads(responseFixtures.text)
+''' FIM FIXTURES '''
 
 # Teste de resposta
-print(response.text)
+print(responsePredictions.text)
+print(responseFixtures.text)
 # print(json.dumps(dados, indent=4, ensure_ascii=False))
-print(dados["response"][0]["predictions"]["winner"]["name"])
+print(dadosPredictions["response"][0]["predictions"]["winner"]["name"])
 
 # Recupera os dados necessários
-timeCasa = dados["response"][0]["teams"]["home"]["name"]
-timeVisitante = dados["response"][0]["teams"]["away"]["name"]
+liga = dadosPredictions["response"][0]["league"]["name"]
+timeCasa = dadosPredictions["response"][0]["teams"]["home"]["name"]
+timeVisitante = dadosPredictions["response"][0]["teams"]["away"]["name"]
+winOrDraw = dadosPredictions["response"][0]["predictions"]["win_or_draw"]
+predictionVencedor = dadosPredictions["response"][0]["predictions"]["winner"]["name"] if winOrDraw else "Empate"
+casaVencedor = dadosFixtures["response"][0]["teams"]["home"]["winner"]
+visitanteVencedor = dadosFixtures["response"][0]["teams"]["away"]["winner"]
+vencedor = timeCasa if casaVencedor and (not visitanteVencedor) else (timeVisitante if visitanteVencedor and (not casaVencedor) else "Empate")
+predictionGolsCasa = dadosPredictions["response"][0]["predictions"]["goals"]["home"]
+golsCasa = dadosFixtures["response"][0]["goals"]["home"]
+predictionGolsVisitante = dadosPredictions["response"][0]["predictions"]["goals"]["away"]
+golsVisitante = dadosFixtures["response"][0]["goals"]["away"]
 
 # Transforma os dados em dataframe
-df = DataFrame({'Casa': [timeCasa], 'Visitante': [timeVisitante]})
+df = DataFrame({'Liga': [liga],
+                'Casa': [timeCasa],
+                'Visitante': [timeVisitante],
+                'Prediction_Vencedor': [predictionVencedor],
+                'Vencedor': [vencedor],
+                'Prediction_Gols_Casa': [predictionGolsCasa],
+                'Gols_Casa': [golsCasa],
+                'Prediction_Gols_Visitante': [predictionGolsVisitante],
+                'Gols_Visitante': [golsVisitante]
+                })
 
 # Exporta o dataframe para excel
-df.to_excel('test.xlsx', sheet_name='sheet1', index=False)
+df.to_excel('partidas.xlsx', sheet_name='sheet1', index=False)
